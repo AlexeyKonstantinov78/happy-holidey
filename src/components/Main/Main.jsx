03fc8fcae75497b5631a5dc94fafbd5a51dad2a2
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-08-30 18:56:22 +0300

    update img

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,14 @@
 import Container from '../Container/Container';
 import style from './Main.module.css';
 
+
 const Main = () => (
   <main className={style.main}>
     <Container>
       <div className={style.card}>
           <div className={style.wrapper}>
             <div className={style.image}>
-              <img className={style.img} src="../img/card-bg.jpg" alt="card-bg" />
+              <img className={style.img} src="img/card-bg.jpg" alt="card-bg" />              
               <p className={style.felicitation}>
                 Поздравляю с днем рожденья!
                 Пусть будет жизнь полна веселья,
